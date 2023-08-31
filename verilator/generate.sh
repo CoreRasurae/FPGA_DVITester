bab706ad53ce49e483284e25a476ed9a4494ff99
CoreRasurae <38363426+CoreRasurae@users.noreply.github.com>
2023-08-31 22:31:16 +0100

    Make generate.sh script more complete

--- verilator/generate.sh
+++ verilator/generate.sh
@@ -1,3 +1,4 @@
 verilator -cc ../common/RGBSigGen.v
 verilator -Wall --trace -cc ../common/RGBSigGen.v --exe tb_RGBSigGen.cpp --timescale-override 1ns/1ns
 make -C obj_dir -f VRGBSigGen.mk VRGBSigGen
+./obj_dir/VRGBSigGen
